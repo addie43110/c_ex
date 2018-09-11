f58c038e9e1818afd17ba02476af4f6254b125d8
Adeline <addie@uvic.ca>
2018-09-11 14:42:21 -0700

    added 5% tax exercise

--- c_ex.c
+++ c_ex.c
@@ -8,9 +8,24 @@
 #include <math.h> /* for pi */
 
 /* declare functions before they are used in main here */
+void printCheck();
 float volume_of_sphere(int radius);
+float add_5_tax(float amount);
 
-
+int main(void) {
+  /*print check */
+	printCheck();
+  /* volume of a sphere */
+  int radius;
+  printf("Enter a radius (integer): ");
+  scanf("%d", &radius);
+  printf("Volume of sphere with %d radius: %f\n", radius, volume_of_sphere(radius));
+	printf("Enter a price and I will calculate the tax (##.##): ");
+	float price;
+	scanf("%f", &price);
+	printf("Plus 5%% tax yields: $%.2f\n", add_5_tax(price));
+	return 0;
+}
 
 void printCheck() {
 	printf("        *\n");
@@ -21,19 +36,10 @@ void printCheck() {
 	printf("  **\n");
 }
 
-int main(void) {
-  /*print check */
-	printCheck();
-  /* volume of a sphere */
-  int radius;
-  printf("Enter a radius (integer): ");
-  scanf("%d", &radius);
-  printf("Volume of sphere with %d radius: %f\n", radius, volume_of_sphere(radius));
-
-	return 0;
-}
-
-
 float volume_of_sphere(int radius){
   return 4.0/3.0*M_PI*radius*radius*radius;
+}
+
+float add_5_tax(float amount){
+	return  amount*1.05;
 }
