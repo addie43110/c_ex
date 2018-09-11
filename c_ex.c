2d8478596574feeb37c5cfdb1fdb581d2e528043
Adeline <addie@uvic.ca>
2018-09-11 14:54:07 -0700

    added print_us_bills()

--- c_ex.c
+++ c_ex.c
@@ -11,6 +11,7 @@
 void printCheck();
 float volume_of_sphere(int radius);
 float add_5_tax(float amount);
+void print_us_bills(int d_amt);
 
 int main(void) {
   /*print check */
@@ -24,6 +25,13 @@ int main(void) {
 	float price;
 	scanf("%f", &price);
 	printf("Plus 5%% tax yields: $%.2f\n", add_5_tax(price));
+
+	/* US dollar bills */
+	int us_d;
+	printf("Enter a U.S. dollar amount (integer): ");
+	scanf("%d", &us_d);
+	print_us_bills(us_d);
+
 	return 0;
 }
 
@@ -42,4 +50,29 @@ float volume_of_sphere(int radius){
 
 float add_5_tax(float amount){
 	return  amount*1.05;
+}
+
+void print_us_bills(int d_amt){
+	int num_20, num_10, num_5, num_1;
+	num_20=0; num_10=0; num_5=0; num_1=0;
+	while(d_amt>20){
+		d_amt = d_amt - 20;
+		num_20 = num_20+1;
+	}
+	printf("$20 bills: %d\n", num_20);
+	while(d_amt>10){
+		d_amt -= 10;
+		num_10 += 1;
+	}
+	printf("$10 bills: %d\n", num_10);
+	while(d_amt>5){
+		d_amt -= 5;
+		num_5 +=1;
+	}
+	printf("$5 bills: %d\n", num_5);
+	while(d_amt>0){
+		d_amt -= 1;
+		num_1 +=1;
+	}
+	printf("$1 bills: %d\n", num_1);
 }
