a7e140628a890e1a1177b71ef0d813c59897b177
Adeline <addie@uvic.ca>
2018-09-10 18:18:14 -0700

    added c_ex.c

---
+++ c_ex.c
@@ -0,0 +1,26 @@
+/* A collection of exercises from the book C Programming: A Modern Approach by K. N. King
+  Taken from Chapter 2
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+
+
+
+
+void printCheck() {
+	printf("        *\n");
+	printf("       *\n");
+	printf("      *\n");
+	printf("*    *\n");
+	printf(" *  *\n");
+	printf("  **\n");
+}
+
+int main(void) {
+	printCheck();
+	return 0;
+}
