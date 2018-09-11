d1211ccbb36b34294f1cbd250d01ff0d0262c110
addie <aedjordon@gmail.com>
2018-09-10 20:00:35 -0700

    added ignore and another problem

--- c_ex.c
+++ c_ex.c
@@ -5,9 +5,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h> /* for pi */
 
-
-
+/* declare functions before they are used in main here */
+float volume_of_sphere(int radius);
 
 
 
@@ -21,6 +22,18 @@ void printCheck() {
 }
 
 int main(void) {
+  /*print check */
 	printCheck();
+  /* volume of a sphere */
+  int radius;
+  printf("Enter a radius (integer): ");
+  scanf("%d", &radius);
+  printf("Volume of sphere with %d radius: %f\n", radius, volume_of_sphere(radius));
+
 	return 0;
+}
+
+
+float volume_of_sphere(int radius){
+  return 4.0/3.0*M_PI*radius*radius*radius;
 }
