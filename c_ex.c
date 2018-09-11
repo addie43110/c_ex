4be07a85b5af339cdac5020a58ad9aa724a840dd
addie <aedjordon@gmail.com>
2018-09-11 15:28:18 -0700

    rewrote us_bills function

--- c_ex.c
+++ c_ex.c
@@ -28,7 +28,7 @@ int main(void) {
 
 	/* US dollar bills */
 	int us_d;
-	printf("Enter a U.S. dollar amount (integer): ");
+	printf("Enter a U.S. dollar amount (integer) and I will tell you the partition of US bills required: ");
 	scanf("%d", &us_d);
 	print_us_bills(us_d);
 
@@ -55,24 +55,23 @@ float add_5_tax(float amount){
 void print_us_bills(int d_amt){
 	int num_20, num_10, num_5, num_1;
 	num_20=0; num_10=0; num_5=0; num_1=0;
-	while(d_amt>20){
-		d_amt = d_amt - 20;
-		num_20 = num_20+1;
-	}
+  
+  /* 20s */
+  num_20 = d_amt/20;
+  d_amt -= num_20*20;
 	printf("$20 bills: %d\n", num_20);
-	while(d_amt>10){
-		d_amt -= 10;
-		num_10 += 1;
-	}
+  
+  /* 10s */
+  num_10 = d_amt/10;
+  d_amt -= num_10*10;
 	printf("$10 bills: %d\n", num_10);
-	while(d_amt>5){
-		d_amt -= 5;
-		num_5 +=1;
-	}
+
+  /* 5s */
+  num_5 = d_amt/5;
+  d_amt -= num_5*5;
 	printf("$5 bills: %d\n", num_5);
-	while(d_amt>0){
-		d_amt -= 1;
-		num_1 +=1;
-	}
+
+  /* 1s */
+  num_1 = d_amt;
 	printf("$1 bills: %d\n", num_1);
 }
