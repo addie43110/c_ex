e4d68d1717142d0ecac24ef430bd221d1d1b4623
addie <aedjordon@gmail.com>
2018-09-14 23:20:09 -0700

    implemented linked list library for ints

--- linkedlist.c
+++ linkedlist.c
@@ -4,6 +4,7 @@
 
 #include "linkedlist.h"
 
+
 /* function to push a node onto the linked list (at the head) */
 void push_head(struct Node **head_ref, int new_pid) {
 
@@ -11,12 +12,22 @@ void push_head(struct Node **head_ref, int new_pid) {
   struct Node * new_node = (struct Node *) malloc(sizeof(struct Node));
   
   /* link the new node to the head*/
-  new_node.pid = new_pid;
+  new_node->pid = new_pid;
   new_node->next = (*head_ref);
 
   /* change head reference */
   (*head_ref) = new_node;
 
+}
+
+/* get the tail */
+struct Node * get_tail(struct Node **head_ref) {
+  struct Node *prev = NULL;
+  for(struct Node *curr=(*head_ref); curr!=NULL; curr=curr->next) {
+    prev = curr;
+  }
+
+  return prev;
 }
 
 
@@ -26,29 +37,77 @@ void push_tail(struct Node **head_ref, int new_pid) {
   
   struct Node * new_node = (struct Node *) malloc(sizeof(struct Node));
 
-  new_node.pid = new_pid;
+  new_node->pid = new_pid;
   new_node->next = NULL;
 
   /* go to the tail of the list */
-  struct Node * curr = (*head_ref);
-  struct Node * prev = NULL;
-  for(struct Node *curr = (*head_ref); curr!=NULL; curr=curr->next) {
-    prev = curr;
-  }
-  prev->next = new_node;
+  struct Node *tail = get_tail(head_ref);
+  tail->next = new_node;
 
 } 
 
 
 struct Node * pop_head(struct Node **head_ref) {
   struct Node * cp_node = (struct Node *) malloc(sizeof(struct Node));
-  cp_node.pid = (*head_ref.pid);
+  cp_node->pid = (*head_ref)->pid;
   cp_node->next = NULL;
 
+  struct Node *og_head = (*head_ref);
 
   /* free head and make new head */
-  struct Node *new_head_ref = (*head_ref)->next;
-  free(*head_ref);
-  head_ref = new_head_ref;
+  *head_ref  = (struct Node *) (*head_ref)->next;
+  free(og_head);
 
+  return cp_node;
+}
+
+struct Node * pop_tail(struct Node **head_ref) {
+  struct Node * cp_node = (struct Node *) malloc (sizeof(struct Node));
+  struct Node *tail = get_tail(head_ref);
+  
+  struct Node *prev = NULL;
+  for(struct Node *curr = (*head_ref); curr->next!=NULL; curr=curr->next) {
+    prev=curr;
+  }
+
+  prev->next = NULL;
+
+  cp_node->pid = tail->pid;
+  cp_node->next = NULL;
+
+  free(tail);
+  return cp_node;  
+
+}
+
+
+struct Node * get(struct Node **head_ref, int ind) {
+  int i = 0;
+  struct Node * cp_node = (struct Node *) malloc(sizeof(struct Node));
+
+  struct Node *prev = NULL;
+  for(struct Node *curr = (*head_ref); curr != NULL; curr=curr->next) {
+      if(i<(ind+1)) {
+        prev = curr;
+        i++;
+      } else {
+        break;
+      }
+  }
+
+  cp_node->pid = prev->pid;
+  cp_node->next = NULL;
+
+  return cp_node; 
+}
+
+void print_list(struct Node **head_ref) {
+  printf("\n{");
+  for(struct Node *curr = (*head_ref); curr!=NULL; curr=curr->next) {
+    printf("%d", curr->pid);
+    if(curr->next!=NULL) {
+      printf(", ");
+    }
+  }
+  printf("}\n");
 }
--- linkedlist.h
+++ linkedlist.h
@@ -12,9 +12,16 @@ struct Node {
 
 void push_head(struct Node **head_ref, int new_pid);
 
+struct Node * get_tail(struct Node **head_ref);
+
 void push_tail(struct Node **head_ref, int new_pid); 
 
 struct Node * pop_head(struct Node **head_ref); 
 
+struct Node * pop_tail(struct Node **head_ref);
+
+struct Node * get(struct Node **head_ref, int i);
+
+void print_list(struct Node **head_ref);
 
 #endif
